8e8745483098e83edd2fc0a8a957a53547c82059
tom-Violence <roujux@gmail.com>
2024-01-04 01:42:10 +0100

    interfaces finies + méthodes de tir OK

--- src/Controller/MjController.php
+++ src/Controller/MjController.php
@@ -34,7 +34,7 @@ return $this->render('mj/roll.html.twig', ['sortie' => $sortie]);
     public function rollForCrit():string
     {
 
-        $critRate = rand(1, 100);
+        $critRate = rand(1, 10);
         $deck = new Deck();
         $dice = new Dice();
         $coin = new Coin();
@@ -46,22 +46,31 @@ return $this->render('mj/roll.html.twig', ['sortie' => $sortie]);
 
         switch ($roulette) {
             case 1:
-                $result = $masterMaster->getName() . "tire une carte et obient le score de " . $masterMaster->deckTirage();
+                $score = $masterMaster->deckTirage();
+                $result = $masterMaster->getName() . "tire une carte et obient le score de " . $score ;
                 break;
             case 2:
-                $result = $masterMaster->getName() . " lance un dé de " . count($masterMaster->getDice()->getFaces()) . " faces et obient le score de " . $masterMaster->diceTirage();
+                $score = $masterMaster->diceTirage();
+                $result = $masterMaster->getName() . " lance un dé de " . count($masterMaster->getDice()->getFaces()) . " faces et obient le score de " . $score ;
                 break;
             case 3:
-                if ($masterMaster->coinTirage() == 1)
-                $result  = $masterMaster->getName() . " lance une pièce " . $masterMaster->getCoin()->getNbLancers() . " fois et obient le score de  100";
-                else
-                $result  = $masterMaster->getName() . " lance une pièce " . $masterMaster->getCoin()->getNbLancers() . " fois et obient le score de  0";
+                if ($masterMaster->coinTirage() == 1) {
+                    $score = 100;
+                }
+                else{
+                    $score = 0;
+
+                }
+                $result = $masterMaster->getName() . " lance une pièce " .
+                    $masterMaster->getCoin()->getNbLancers() . " fois et obient le score de " . $score;
 
                 break;
             default :
                 return ("oops, il ne s'est rien passé, le maitre doit dormir...");
 
         }
-return  $result;
+if ($score > $critRate) return $result . ", le critrate était de " . $critRate . ". PapixJux est trop fort.";
+elseif ($score < $critRate)  return $result . ", le critrate était de " . $critRate . ". PapixJux est trop nul.";
+else return $result . ", le critrate était de " . $critRate . ". C'est un coup pour rien.";
     }
 }
