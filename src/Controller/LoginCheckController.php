89485281c033fd584e60c66d019ce4795fe82053
Mickael-6 <mickael.hid@gmail.com>
2024-01-10 16:35:22 +0100

    commit login check

---
+++ src/Controller/LoginCheckController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LoginCheckController extends AbstractController
+{
+    #[Route('/login/check', name: 'app_login_check')]
+    public function index(): Response
+    {
+        return $this->render('login_check/index.html.twig', [
+            'controller_name' => 'LoginCheckController',
+        ]);
+    }
+}
