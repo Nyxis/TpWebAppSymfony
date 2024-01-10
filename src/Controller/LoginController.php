8ba53ed9d997588b7731eb1becf674ecf5e07484
claire <115070408+Clairevitsing@users.noreply.github.com>
2024-01-10 16:28:20 +0100

    create a login form

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\LoginFormType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+class LoginController extends AbstractController
+{
+    #[Route('/login', name: 'app_login')]
+    public function index(Request $request): Response
+    {
+        $form = $this->createForm(LoginFormType::class);
+
+            $form->handleRequest($request);
+
+            if ($form->isSubmitted() && $form->isValid()) {
+
+                return $this->redirectToRoute('home');
+            }
+
+            return $this->render('login/index.html.twig', [
+                'form' => $form->createView(),
+            ]);
+        }
+}
