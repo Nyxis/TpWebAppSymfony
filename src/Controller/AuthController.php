58d629c650d5a816fd483ab35bd55485abf80318
tom-Violence <roujux@gmail.com>
2024-01-11 14:11:02 +0100

    "security config"

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -2,44 +2,42 @@
 
 namespace App\Controller;
 
-
-use App\Entity\LoggerClass;
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class AuthController extends AbstractController
 {
     #[Route(path: '/auth/login')]
-    public function login(Request $request): Response
+    public function login(FormFactoryInterface $formFactory, Request $request): Response
     {
+//        dump($authenticationUtils->getLastAuthenticationError());
 
-        $loggerObj = new LoggerClass();
-
-        $form = $this->createFormBuilder($loggerObj)
+        $form = $formFactory->createNamedBuilder('')
             ->setMethod('POST')
             ->setAction('/auth/login_check')
-            ->add('username', EmailType::class)
-            ->add('password', PasswordType::class)
+            ->add('_username', EmailType::class)
+            ->add('_password', PasswordType::class)
             ->add('login', SubmitType::class, ['label' => 'se connecter'])
             ->getForm();
 
         return $this->render('/auth/login.html.twig', ['form' => $form->createView()]);
-
     }
 
     #[Route(path: '/auth/login_check')]
     public function loginCheck(Request $request): Response
     {
-
+        printf("in the Checkcontroller");
         if ($request->isMethod('POST')) return $this->render('/auth/success.html.twig', []);
         else return $this->render('/auth/login.html.twig', []);
-
 
     }
 
---
+++ src/Entity/User.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Entity;
+
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class User implements UserInterface, PasswordAuthenticatedUserInterface
+{
+
+    protected string $eMail;
+    protected string $password;
+    public function __construct()
+    {
+
+    }
+
+
+
+    public function getEMail(): string
+    {
+        return $this->eMail;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function getRoles(): array
+    {
+        // TODO: Implement getRoles() method.
+        return [ 'ROLE_ADMIN' ] ;
+    }
+
+    public function eraseCredentials(): void
+    {
+        // TODO: Implement eraseCredentials() method.
+        return;
+    }
+
+    public function getUserIdentifier(): string
+    {
+        return $this->eMail ;
+    }
+
+}
