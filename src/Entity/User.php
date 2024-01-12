f674ec540bce9bf29f0d61465e10f2ecfa718a23
EsoraKyugo <tom.7trouillet@gmail.com>
2024-01-12 09:49:47 +0100

    j'ai rien compris mais j'essaye quand meme

---
+++ src/Entity/User.php
@@ -0,0 +1,28 @@
+// src/Entity/User.php
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+class User implements UserInterface, PasswordAuthenticatedUserInterface
+{
+
+    {
+    // ...
+
+    /**
+     * @return string the hashed password for this user
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+}
+    password_hashers:
+        # Use native password hasher, which auto-selects and migrates the best
+        # possible hashing algorithm (which currently is "bcrypt")
+        Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface: 'auto'
+}
