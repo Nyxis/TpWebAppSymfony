5dcc71aa82a0e3b27f0c0ac528b77d1e659efbcf
Mickael-6 <mickael.hid@gmail.com>
2024-01-14 21:07:23 +0100

    commit emailType

--- src/Form/Type/UserType.php
+++ src/Form/Type/UserType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 
@@ -19,7 +20,7 @@ class UserType extends AbstractType
             ->add('firstname', TextType::class)
             ->add('lastname', TextType::class)
             ->add('password', PasswordType::class)
-            ->add('email', TextType::class)
+            ->add('email', EmailType::class)
             ->add('roles', ChoiceType::class,[
                 'choices'=>[
                     'ROLE_USER' => 'user.role.user',
