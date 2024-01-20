7e01c4f1cc6b263c7a1d56a9bef71ffb3626dd43
claire <115070408+Clairevitsing@users.noreply.github.com>
2024-01-21 00:25:04 +0100

    make the tailwind style for all templates

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,13 +2,12 @@
 
 namespace App\Controller;
 
-use App\Form\LoginFormType;
 use App\Security\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
 
 
 class LoginController extends AbstractController
@@ -20,11 +19,11 @@ class LoginController extends AbstractController
 
     }
 
-    #[Route('/admin/loginCheck', name: 'app_login_check')]
+    #[Route('/admin/loginCheck', name: 'app_login_check', methods: ['POST'])]
     public function loginCheck():Response
     {
 
-        return $this->render('admin/login/LoginCheck.html.twig');
+        return $this->render('admin/login/loginSuccess.html.twig');
     }
 
 }
--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -40,4 +40,5 @@ class UserRepository extends ServiceEntityRepository
 
         return $query->getResult();
     }
+
 }
